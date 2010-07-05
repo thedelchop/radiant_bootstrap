e3d56366cf8dede2323d1ca64b86a9834e3887b9
Joseph DelCioppio <Joseph.DelCioppio@synantus.com>
2010-07-05 11:49:40 -0400

    Add ActionMailer to the frameworks that are used in the project

--- config/environment.rb
+++ config/environment.rb
@@ -10,10 +10,6 @@ require File.join(File.dirname(__FILE__), 'boot')
 require 'radius'
 
 Radiant::Initializer.run do |config|
-  # Skip frameworks you're not going to use (only works if using vendor/rails).
-  # To use Rails without a database, you must remove the Active Record framework
-  config.frameworks -= [ :action_mailer ]
-
   # Only load the extensions named here, in the order given. By default all
   # extensions in vendor/extensions are loaded, in alphabetical order. :all
   # can be used as a placeholder for all extensions not explicitly named.
